0622b91078815d01b0f7cf96712fb3a12fb7ba82
Matheus Fernandes <matheusvinicius7000@gmail.com>
2024-07-18 23:04:52 -0300

    .gitignore: Add idea/ folder

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+
+class Main:
+    def __init__(self):
+        self.initialize()
+        self.load_rom(sys.argv[1])
+        while not self.has_exit:
+            self.dispatch_events()
+            self.cycle()
+            self.draw()
+
+    def initialize(self):
+        pass
+
+    def load_rom(self, filename):
+        pass
+
+    def dispatch_events(self):
+        pass
+
+    def cycle(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def has_exit(self):
+        pass
