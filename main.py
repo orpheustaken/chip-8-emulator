4049df3fe131704363b88dca89459019b5d95f74
Matheus Fernandes <matheusvinicius7000@gmail.com>
2024-07-20 01:04:17 -0300

    Window: Run sample Pyglet window

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
 import sys
+import os
 
 from domain.cpu import CPU
 from domain.font import Font
 from domain.input import Input
 from domain.memory import Memory
 from domain.output import Output
+from domain.window import Window
 
 
 class Main:
     def __init__(self):
-        self.initialize()
+        app_cpu = CPU()
+        app_font = Font()
+        app_input = Input()
+        app_memory = Memory()
+        app_output = Output()
+        app_window = Window(800, 600, "CHIP-8")
+
         self.load_rom(sys.argv[1])
-        self.load_fonts
+        self.load_fonts()
+
         while not self.has_exit:
-            self.dispatch_events()
-            self.cycle()
-            self.draw()
+            app_window.dispatch_events()
+            app_cpu.cycle()
+            app_window.run()
 
-    def initialize(self):
-        self.clear()
-        cpu = CPU()
-        font = Font()
-        key_input = Input()
-        memory = Memory()
-        output = Output()
-
-    def load_rom(self):
+    @staticmethod
+    def load_rom(rom):
+        # if os.path.exists(rom):
         pass
 
     def load_fonts(self):
         pass
 
-    def dispatch_events(self):
-        pass
-
-    # TODO: move to CPU class?
-    def cycle(self):
-        pass
-
-    # TODO: move draw to Output class?
-    def draw(self):
-        pass
-
     def has_exit(self):
         pass
 
-    # clear pyglet window
-    def clear(self):
-        pass
+    @staticmethod
+    def run():
+        Window.run()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("ROM must be provided to be run by the CHIP-8 interpreter")
+        print("Usage: main.py <rom_path>")
+        exit(1)
+
+    app = Main()
+    app.run()
