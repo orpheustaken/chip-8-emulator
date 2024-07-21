9e8899fff87783ff70d349e2c60627027863e6af
Matheus Fernandes <matheusvinicius7000@gmail.com>
2024-07-21 10:45:48 -0300

    Window: Adding some missing methods

--- domain/window.py
+++ domain/window.py
@@ -8,7 +8,7 @@ class Window(pyglet.window.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         self.label = pyglet.text.Label(
-            "CHIP-8 Emulator",
+            "CHIP-8",
             font_name="Roboto",
             font_size=36,
             x=self.width // 2,
@@ -20,15 +20,26 @@ class Window(pyglet.window.Window):
         self.rectangle = shapes.Rectangle(
             200, 150, 400, 300, color=(255, 255, 255), batch=self.batch
         )
-        # update screen only when needed
-        self.should_draw = False
+        # TODO: update screen only when needed
+        self.should_draw = True
 
     def on_draw(self):
-        self.clear()
-        self.label.draw()
-        self.batch.draw()
+        if self.should_draw:
+            self.clear()
+            self.label.draw()
+            self.batch.draw()
+
+    def on_close(self):
+        pyglet.app.exit()
+        return super().on_close()
 
     def dispatch_events(self):
+        pass
+
+    def on_key_press(self, symbol, modifiers):
+        pass
+
+    def on_key_release(self, symbol, modifiers):
         pass
 
     @staticmethod
