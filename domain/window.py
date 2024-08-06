f4cec7d5cace82a7fd4a11d5cc887e333e8462cc
Matheus Fernandes <matheusvinicius7000@gmail.com>
2024-08-06 18:47:49 -0300

    Window: Fix dictionary access by converting to List

--- domain/window.py
+++ domain/window.py
@@ -5,61 +5,67 @@ from pyglet import shapes
 
 # TODO: there are more abstract methods from pyglet's superclass to be implemented
 class Window(pyglet.window.Window):
-    def __init__(self, width, height, title, keys):
+    def __init__(self, width, height, title, keys, display_buffer):
         super().__init__(width, height, title)
 
-        # self.label = pyglet.text.Label(
-        #     "CHIP-8",
-        #     font_name="Roboto",
-        #     font_size=36,
-        #     x=self.width // 2,
-        #     y=self.height // 2,
-        #     anchor_x="center",
-        #     anchor_y="center",
-        # )
-        # self.batch = pyglet.graphics.Batch()
-        # self.rectangle = shapes.Rectangle(
-        #     200, 150, 400, 300, color=(255, 255, 255), batch=self.batch
-        # )
+        self.label = pyglet.text.Label(
+            "CHIP-8 Emulator",
+            font_name="Roboto",
+            font_size=24,
+            x=self.width // 2,
+            y=self.height // 2,
+            anchor_x="center",
+            anchor_y="center",
+        )
 
-        self.should_draw = False
+        # both should false by default
+        self.should_draw = True
         self.key_wait = False
 
         # pseudo-pixelwise drawing with 10x10 boxes
+        # self.pixel = pyglet.image.SolidColorImagePattern(color=(255, 255, 255, 255)).create_image(10, 10)
         self.pixel = pyglet.resource.image('assets/pixel.png')
         self.buzz = pyglet.resource.media('assets/buzz.wav', streaming=False)
 
         # "pixel" buffer
         self.batch = pyglet.graphics.Batch()
-        self.sprites = []
+        self.sprites = [2048]
 
+        # fills each output pixel
         for i in range(0, 2048):
             self.sprites.append(pyglet.sprite.Sprite(self.pixel, batch=self.batch))
 
+            # sprite = pyglet.sprite.Sprite(self.pixel, batch=self.batch)
+            # sprite.x = (i % 64) * 10  # 64 sprites per row, each 10 pixels wide
+            # sprite.y = (i // 64) * 10  # Each new row starts at y = (row number) * 10
+            # self.sprites.append(sprite)
+
         self.KEY_MAP = {
-            pyglet.window.key._1: keys.values(0),
-            pyglet.window.key._2: keys.values(1),
-            pyglet.window.key._3: keys.values(2),
-            pyglet.window.key._4: keys.values(3),
-            pyglet.window.key.Q: keys.values(4),
-            pyglet.window.key.W: keys.values(5),
-            pyglet.window.key.E: keys.values(6),
-            pyglet.window.key.R: keys.values(7),
-            pyglet.window.key.A: keys.values(8),
-            pyglet.window.key.S: keys.values(9),
-            pyglet.window.key.D: keys.values(10),
-            pyglet.window.key.F: keys.values(11),
-            pyglet.window.key.Z: keys.values(12),
-            pyglet.window.key.X: keys.values(13),
-            pyglet.window.key.C: keys.values(14),
-            pyglet.window.key.V: keys.values(15),
+            pyglet.window.key._1: list(keys.values())[0],
+            pyglet.window.key._2: list(keys.values())[1],
+            pyglet.window.key._3: list(keys.values())[2],
+            pyglet.window.key._4: list(keys.values())[3],
+            pyglet.window.key.Q: list(keys.values())[4],
+            pyglet.window.key.W: list(keys.values())[5],
+            pyglet.window.key.E: list(keys.values())[6],
+            pyglet.window.key.R: list(keys.values())[7],
+            pyglet.window.key.A: list(keys.values())[8],
+            pyglet.window.key.S: list(keys.values())[9],
+            pyglet.window.key.D: list(keys.values())[10],
+            pyglet.window.key.F: list(keys.values())[11],
+            pyglet.window.key.Z: list(keys.values())[12],
+            pyglet.window.key.X: list(keys.values())[13],
+            pyglet.window.key.C: list(keys.values())[14],
+            pyglet.window.key.V: list(keys.values())[15],
         }
 
     def on_draw(self):
         if self.should_draw:
             self.clear()
-            # self.label.draw()
-            # self.batch.draw()
+            self.label.draw()
+            self.batch.draw()
+            self.flip()
+            self.should_draw = False
 
     def on_close(self):
         pyglet.app.exit()
